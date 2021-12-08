1bc3b85794fbd650075cd4a416f0c9740becc41c
darkhorseMMXXII <95745633+darkhorseMMXXII@users.noreply.github.com>
2021-12-08 20:13:38 +0200

    init commit

---
+++ Program.cs
@@ -0,0 +1,85 @@
+﻿
+
+using Microsoft.ML;
+using ObjectDetection.YoloParser;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+
+static string GetAbsolutePath(string relativePath)
+{
+    FileInfo _dataRoot = new FileInfo(typeof(Program).Assembly.Location);
+    string assemblyFolderPath = _dataRoot.Directory.FullName;
+
+    string fullPath = Path.Combine(assemblyFolderPath, relativePath);
+
+    return fullPath;
+}
+
+var assetsRelativePath = @"../../../assets";
+string assetsPath = GetAbsolutePath(assetsRelativePath);
+var modelFilePath = Path.Combine(assetsPath, "Model", "TinyYolo2_model.onnx");
+var imagesFolder = Path.Combine(assetsPath, "images");
+var outputFolder = Path.Combine(assetsPath, "images", "output");
+
+MLContext mlContext = new MLContext();
+
+void DrawBoundingBox(string inputImageLocation, string outputImageLocation, string imageName, IList<YoloBoundingBox> filteredBoundingBoxes)
+{
+    Image image = Image.FromFile(Path.Combine(inputImageLocation, imageName));
+
+    var originalImageHeight = image.Height;
+    var originalImageWidth = image.Width;
+
+    foreach (var box in filteredBoundingBoxes)
+    {
+        var x = (uint)Math.Max(box.Dimensions.X, 0);
+        var y = (uint)Math.Max(box.Dimensions.Y, 0);
+        var width = (uint)Math.Min(originalImageWidth - x, box.Dimensions.Width);
+        var height = (uint)Math.Min(originalImageHeight - y, box.Dimensions.Height);
+
+        string text = $"{box.Label} ({(box.Confidence * 100).ToString("0")}%)";
+
+        using (Graphics thumbnailGraphic = Graphics.FromImage(image))
+        {
+            thumbnailGraphic.CompositingQuality = CompositingQuality.HighQuality;
+            thumbnailGraphic.SmoothingMode = SmoothingMode.HighQuality;
+            thumbnailGraphic.InterpolationMode = InterpolationMode.HighQualityBicubic;
+
+            // Define Text Options
+            Font drawFont = new Font("Arial", 12, FontStyle.Bold);
+            SizeF size = thumbnailGraphic.MeasureString(text, drawFont);
+            SolidBrush fontBrush = new SolidBrush(Color.Black);
+            Point atPoint = new Point((int)x, (int)y - (int)size.Height - 1);
+
+            // Define BoundingBox options
+            Pen pen = new Pen(box.BoxColor, 3.2f);
+            SolidBrush colorBrush = new SolidBrush(box.BoxColor);
+
+            thumbnailGraphic.FillRectangle(colorBrush, (int)x, (int)(y - size.Height - 1), (int)size.Width, (int)size.Height);
+            thumbnailGraphic.DrawString(text, drawFont, fontBrush, atPoint);
+
+            // Draw bounding box on image
+            thumbnailGraphic.DrawRectangle(pen, x, y, width, height);
+
+        }
+    }
+
+    if (!Directory.Exists(outputImageLocation))
+    {
+        Directory.CreateDirectory(outputImageLocation);
+    }
+
+    image.Save(Path.Combine(outputImageLocation, imageName));
+}
+
+static void LogDetectedObjects(string imageName, IList<YoloBoundingBox> boundingBoxes)
+{
+    Console.WriteLine($".....The objects in the image {imageName} are detected as below....");
+
+    foreach (var box in boundingBoxes)
+    {
+        Console.WriteLine($"{box.Label} and its Confidence score: {box.Confidence}");
+    }
+
+    Console.WriteLine("");
+}
